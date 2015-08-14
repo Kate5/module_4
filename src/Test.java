dd0ca00da5006911f188872bdff65750afa3a41b
kateryna.maksimenko <kateryna.maksimenko@gmail.com>
2015-08-14 22:23:21 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Created by backendlessdev on 8/14/15.
+ */
+public class Test
+{
+  public static void main( String[] args )
+  {
+
+    int i = 1;
+
+    if( i > 10 )
+    {
+
+      System.out.println( "i is bigger than 10" );
+    }
+
+    else
+    {
+
+      System.out.println( "i less than 10" );
+    }
+  }
+}
